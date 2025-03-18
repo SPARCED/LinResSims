1359c72d934d0386a96bf7341c3b842e60eb80b2
arnabmutsuddy <amutsud@g.clemson.edu>
2025-03-17 20:04:39 -0400

    time point rounding

--- bin/modules/RunTyson.py
+++ bin/modules/RunTyson.py
@@ -8,6 +8,19 @@ Created on Sat Jan 25 20:12:08 2025
 import numpy as np
 from scipy.integrate import solve_ivp
 from modules.TysonModule import system_of_odes
+import math
+
+def round_tp(num):
+    
+    remainder = num - math.floor(num)
+    if remainder > 0 and remainder < 0.5:
+        remainder = 0
+    elif remainder >= 0.5 and remainder < 1.0:
+        remainder = 0.5
+    
+    new_num = math.floor(num) + remainder
+    
+    return new_num
 
 
 def RunTyson(th,spdata,params):
@@ -18,7 +31,11 @@ def RunTyson(th,spdata,params):
     
     n_tp = int(t_span[1]*2+1)
     
-    solution = solve_ivp(system_of_odes, t_span, spdata, t_eval=np.linspace(t_span[0], t_span[1], n_tp), args=(params,),method='LSODA')
+    tp_cell = np.linspace(t_span[0], t_span[1], n_tp)
+    
+    tp_rounded = [round_tp(tp) for tp in tp_cell]
+    
+    solution = solve_ivp(system_of_odes, t_span, spdata, t_eval=tp_rounded, args=(params,),method='LSODA')
     t = solution.t
     y = solution.y
     
